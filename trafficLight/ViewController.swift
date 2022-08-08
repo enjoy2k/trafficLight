3f12482c5788fc06eb4b966ae369544b7a72701d
DaniilKozlov <enjoy2k@mail.ru>
2022-08-08 18:00:09 +0300

    Написал первое действие с кнопки

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightSignal: UIView!
+    @IBOutlet var yellowLightSignal: UIView!
+    @IBOutlet var greenLightSignal: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightSignal.alpha = 0.3
+        yellowLightSignal.alpha = 0.3
+        greenLightSignal.alpha = 0.3
+        
+        redLightSignal.layer.cornerRadius = redLightSignal.frame.size.width/2
+        yellowLightSignal.layer.cornerRadius = redLightSignal.frame.size.width/2
+        greenLightSignal.layer.cornerRadius = redLightSignal.frame.size.width/2
+        
+        startButton.layer.cornerRadius = 15
     }
 
-
+    @IBAction func StartButtonPressed() {
+        startButton.setTitle("Next", for: .normal)
+        redLightSignal.alpha = 1
+    }
+    
 }
 
