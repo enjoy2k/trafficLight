b22e0d30e345a3a3a8f6d78a8e32dd1652efb3f5
enjoy2k <Enjoy2k@mail.ru>
2022-09-09 18:16:33 +0300

    Переделал решение, так как делал преподаватель. Узнал что лучше создать отдельные свойства, для значений прозрачности света сигналов

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+enum ColorLight {  // Если перечисление может быть доступно для изменения, тогда его можно вынести за пределы класса(Наверх). Внутри класса оно должно быть приватным.
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet var redLightSignal: UIView!
@@ -15,81 +21,43 @@ class ViewController: UIViewController {
     
     @IBOutlet var startButton: UIButton!
     
-//    var lightSwitcher = "Red" // Свойство должен был указать вначале
     private var lightSwitcher = ColorLight.red
+    private let lightIsOn: CGFloat = 1 // Свойство сделано для того чтобы не прописывать каждый раз руками
+    private let lightIsOff: CGFloat = 0.3 // Очень классный пример привели: Выключатель. Либо так либо так
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLightSignal.alpha = 0.3
-        yellowLightSignal.alpha = 0.3
-        greenLightSignal.alpha = 0.3
-        
-    }
-//    helllo!!
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        redLightSignal.layer.cornerRadius = redLightSignal.frame.width/2
-        yellowLightSignal.layer.cornerRadius = redLightSignal.frame.width/2
-        greenLightSignal.layer.cornerRadius = redLightSignal.frame.width/2
-        
         startButton.layer.cornerRadius = 15
+        
+        redLightSignal.alpha = lightIsOff
+        yellowLightSignal.alpha = lightIsOff
+        greenLightSignal.alpha = lightIsOff
     }
-    
-//    @IBAction func startButtonPressed() {
-//        switch lightSwitcher {
-//        case "Red" :
-//            startButton.setTitle("Next", for: .normal)
-//            redLightSignal.alpha = 1
-//            lightSwitcher = "Yellow"
-//        case "Yellow" :
-//            redLightSignal.alpha = 0.3
-//            yellowLightSignal.alpha = 1
-//            lightSwitcher = "Green"
-//        case "Green" :
-//            yellowLightSignal.alpha = 0.3
-//            greenLightSignal.alpha = 1
-//            lightSwitcher = "Back to start"
-//        case "Back to start" :
-//            greenLightSignal.alpha = 0.3
-//            startButton.setTitle("Start", for: .normal)
-//            lightSwitcher = "Red"
-//        default:
-//            break
-//        }
-//    }
-    private enum ColorLight {  // Если перечисление может быть доступно для изменения, тогда его можно вынести за пределы класса(Наверх). Внутри класса оно должно быть приватным.
-        case red
-        case yellow
-        case green
-        case backToStart
+
+    override func viewWillLayoutSubviews() {
+        redLightSignal.layer.cornerRadius = redLightSignal.frame.width / 2
+        yellowLightSignal.layer.cornerRadius = redLightSignal.frame.width / 2
+        greenLightSignal.layer.cornerRadius = redLightSignal.frame.width / 2
     }
     
     @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
         switch lightSwitcher {
         case .red:
-            startButton.setTitle("Next", for: .normal)
-            redLightSignal.alpha = 1
+            greenLightSignal.alpha = lightIsOff
+            redLightSignal.alpha = lightIsOn
             lightSwitcher = .yellow
         case .yellow:
-            redLightSignal.alpha = 0.3
-            yellowLightSignal.alpha = 1
+            redLightSignal.alpha = lightIsOff
+            yellowLightSignal.alpha = lightIsOn
             lightSwitcher = .green
         case .green:
-            yellowLightSignal.alpha = 0.3
-            greenLightSignal.alpha = 1
-            startButton.setTitle("Go!", for: .normal)
-            lightSwitcher = .backToStart
-        case .backToStart:
-            greenLightSignal.alpha = 0.3
-            startButton.setTitle("Start", for: .normal)
+            yellowLightSignal.alpha = lightIsOff
+            greenLightSignal.alpha = lightIsOn
             lightSwitcher = .red
         }
     }
 }
-// Либо так:
-/* extension ViewController { Расширение для класса viewController
-    private enum ColorLight { Здесь он  так же приватный
-case red, yellow, green, backToStart
-} */
